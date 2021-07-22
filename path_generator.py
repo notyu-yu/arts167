2fa177500a73c4b7d8f140d85368ad7952af0d56
Yuhang <yuhangcui.yc@gmail.com>
2021-07-21 20:56:10 -0400

    first commit

---
+++ path_generator.py
@@ -0,0 +1,15 @@
+import os
+
+file_lst = []
+
+#Referenced https://mkyong.com/python/python-how-to-list-all-files-in-a-directory/ for getting file paths
+for r, d, f in os.walk('.'):
+    for file in f:
+        if '.html' in file:
+            file_lst.append(os.path.join(r,file)[2:])
+
+with open('files.txt','w+') as f:
+    f.write('<ul>\n')
+    for file in file_lst:
+        f.write(f'\t<li><a href="{file}" target="_blank">_____</a></li>\n')
+    f.write('</ul>')
